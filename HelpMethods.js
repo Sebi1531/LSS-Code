a348c3c0b0b2fba59d76de98109d24403b62b7fb
Sebi1531 <33638099+Sebi1531@users.noreply.github.com>
2023-12-23 00:40:05 +0100

    Update HelpMethods.js
    rework func checkVersion

--- HelpMethods.js
+++ HelpMethods.js
@@ -1,3 +1,29 @@
-function checkVersion(url) {
-  console.log("Dummy-Output");
+function checkVersion(scriptName, actVersion, scriptURL) {
+    let searched = '@version';
+    let versLine;
+    try {
+        let response = await fetch(scriptURL);
+        if (!response.ok) {
+            throw new Error("ERROR: '" + scriptURL + "' not reachable.");
+        }
+    
+        const code = await response.text();
+        const lineArr = code.split('\n');
+        versLine = lineArr.find(line => line.includes(searched));
+        if (versLine === undefined) {
+            throw new Error("ERROR: version not found (link: '" + scriptURL + "')");
+        }
+    } catch (error) {
+        console.error('Fehler beim Abrufen des JS-Dokuments:', error);
+    }
+
+    let newestVersion = versLine.split(searched)[1].replaceAll(' ', '');
+    if (actVersion != newestVersion) {
+        let divNode = document.createElement("div");
+        divNode.className = "alert alert-info";
+        divNode.addChild(document.createTextNode("Beim Skript '" + scriptName + "' steht eine neue Version zu Verfügung. Neueste Version: " + newestVersion));
+        divNode.addChild(document.createElement("br"));
+        divNode.addChild(document.createTextNode("Es wird empfohlen, die neueste Version herunterladen, weil veraltete Versionen eventuell nicht mehr funktionieren."));
+        document.insertBefore(divNode, document.firstChild);
+    }
 }
